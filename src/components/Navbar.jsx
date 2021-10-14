72d7c140b6d444802ba8e0872baf6cb9397f2138
DiegaG <diego.garciasanchez@gmail.com>
2021-10-13 21:48:48 -0300

    navbar con reacstrap

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+/*  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+ NavbarText*/
+} from 'reactstrap';
+
+const Menu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">Diamond Solutions</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/notebooks/">Notebooks</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/placas/">Placas de Video</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/placas/">Fuentes</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/placas/">PC Gamer</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Menu;
